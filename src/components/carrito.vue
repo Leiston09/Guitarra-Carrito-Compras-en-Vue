22c387894ee72fdcbf8e555fb3f04d211763ffdf
LeistonH <232432491+Leiston09@users.noreply.github.com>
2026-01-31 14:22:10 -0500

    Creacion de Props y Emits con v-for

--- src/components/carrito.vue
+++ src/components/carrito.vue
@@ -83,55 +83,12 @@
 
         <img class="header-guitarra" src="/img/header_guitarra.png" alt="imagen header">
     </header>
-
-    <main class="container-xl mt-5">
-        <h2 class="text-center">Nuestra Colección</h2>
-
-        <div class="row mt-5">
-           <guitarra
-             v-for="guitarra in guitarras"   
-             v-bind:guitarra = "guitarra"
-             @agregar-carrito="agregarCarrito"
-           /> 
-        </div>
-    </main>
-
-
-    <footer class="bg-dark mt-5 py-5">
-        <div class="container-xl">
-            <p class="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-        </div>
-    </footer>
 </template>
 
 <script setup>
 
-import {ref , reactive , onMounted} from "vue"
-import { db } from "./data/guitarras";
-import guitarra from "./components/guitarra.vue";
-
-//const state = reactive({
-//    guitarra : db,
-//})
-
-
-    const guitarras = ref([])
-    const carrito = ref([])
-
-    onMounted(function(){
-        guitarras.value = db
-    })
-
-
-    const agregarCarrito = (guitarra) => {
-        guitarra.cantidad = 1
-        carrito.value.push(guitarra)
-        console.log(guitarra)
-    }
-
-
 </script>
 
-<style   scoped>
+<style lang="scss" scoped>
 
 </style>
---
+++ src/components/guitarra.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div class="col-4">
+            <img 
+                class="img-fluid" 
+                v-bind:src=" '/img/' + guitarra.imagen + '.jpg'" 
+                v-bind:alt="'imagen guitarra' + guitarra.nombre"
+            >
+        </div>
+        <div class="col-8">
+            <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+            <p>{{ guitarra.descripcion }}</p>
+            <p class="fw-black text-primary fs-3">${{guitarra.precio}}</p>
+            <button 
+                type="button"
+                class="btn btn-dark w-100 "
+                @click="$emit('agregarCarrito', guitarra)"
+            >Agregar al Carrito</button>
+        </div>
+    </div><!-- FIN GUITARRA -->
+</template>
+
+<script setup>
+
+    const props = defineProps({
+        guitarra: {
+            type: Object,
+            required: true
+        }
+    })
+    
+    const emits = defineEmits(['agregarCarrito'])
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
